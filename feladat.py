7796e8b2365585ac4b2e0d60fa3673810b4e778e
BalazsTeodora <144986713+BalazsTeodora@users.noreply.github.com>
2023-09-21 08:19:35 +0200

    Add files via upload

---
+++ feladat.py
@@ -0,0 +1,85 @@
+# feladat kérjünk be a felhasználótól kettő egész számot és vizsgáljuk meg EZEKRŐL
+#MELYIK A NAGYOBB ÉRTÉK ILLETVE HA EGYENLŐ
+
+#2. bekérünk 5 egész számot mindegyik pozitiv vagy negatív 20-nál nagyobb vagy kisebb
+#számold meg hány van az 5 közül ami pozitiv és még nagyobb is
+
+"""
+#1. feladat
+
+szam1 = int(input("Kérlek adj meg egy számot:"))
+szam2 = int(input("Kérlek adj meg egy számot:"))
+if szam1 >szam2:
+    print("az első szám nagyobb")
+elif szam1 < szam2:
+    print("a második szám a nagyobb")
+elif szam1 == szam2:
+    print("a két szám egyenlő")
+"""
+szam3 = int(input("Kérlek adj meg egy számot:"))
+szam4 = int(input("Kérlek adj meg egy számot:"))
+szam5 = int(input("Kérlek adj meg egy számot:"))
+szam6 = int(input("Kérlek adj meg egy számot:"))
+szam7 = int(input("Kérlek adj meg egy számot:"))
+
+num = num+1
+
+
+if szam3 > 0:
+    print("a megadott szám pozitív")
+if szam3 < 0:
+    print("a megadott szám negatív")
+if szam3 < 20:
+    print("a szám kisebb mint 20")
+if szam3 > 20:
+    print("a szám nagyobb mint 20")
+    
+if szam4 > 0:
+    print("a megadott szám pozitív")
+if szam4 < 0:
+    print("a megadott szám negatív")
+if szam4 < 20:
+    print("a szám kisebb mint 20")
+if szam4 > 20:
+    print("a szám nagyobb mint 20")
+    
+    
+if szam5 > 0:
+    print("a megadott szám pozitív")
+if szam5 < 0:
+    print("a megadott szám negatív")
+if szam5 < 20:
+    print("a szám kisebb mint 20")
+if szam5 > 20:
+    print("a szám nagyobb mint 20")
+    
+if szam6 > 0:
+    print("a megadott szám pozitív")
+if szam6 < 0:
+    print("a megadott szám negatív")
+if szam6 < 20:
+    print("a szám kisebb mint 20")
+if szam6 > 20:
+    print("a szám nagyobb mint 20")
+    
+if szam7 > 0:
+    print("a megadott szám pozitív")
+if szam7 < 0:
+    print("a megadott szám negatív")
+if szam7 < 20:
+    print("a szám kisebb mint 20")
+if szam7 > 20:
+    print("a szám nagyobb mint 20")
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
